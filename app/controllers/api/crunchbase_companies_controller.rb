a969854334107fc30607f7d0380cf83e72b004c5
Alessio Fanelli <fana@alessiofanelli.com>
2021-08-17 11:22:02 -0700

    Add total items count to API response

--- app/controllers/api/crunchbase_companies_controller.rb
+++ app/controllers/api/crunchbase_companies_controller.rb
@@ -1,5 +1,5 @@
 class Api::CrunchbaseCompaniesController < ApplicationController
   def index
-    render json: { crunchbase_companies: paginate(CrunchbaseCompany.all) }
+    render json: { crunchbase_companies: paginate(CrunchbaseCompany.all), count: CrunchbaseCompany.count }
   end
 end
