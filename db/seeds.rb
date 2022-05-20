6383c2c9258ff3e05c60b6421ea4abfc81ed9906
lexicalbits <justin@lexicalbits.com>
2022-05-20 16:23:15 -0400

    Fixed a few settings and packages to allow install to proceed smoothly (#1)
    * Fixed a few settings and packages to allow install to proceed smoothly
    
    * Installation instructions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,12 @@ class_file_pairings = {
   'crunchbase-investments' => CrunchbaseInvestment
 }.freeze
 
+p "Loading Crunchbase data (this could take a while!)"
+
 class_file_pairings.each do |file_name, klass|
+  p "...#{file_name}"
   rows = CSV.parse(File.read(Rails.root.join("db/bulk_data/#{file_name}.csv")).scrub, headers: true)
   rows.each { |row| klass.find_or_create_by(row.to_h) }
 end
+
+p "Phew, all done!"
